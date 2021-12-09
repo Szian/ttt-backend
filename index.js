1a643364f338a5df2a4316881f38753f6f242b54
Zihan Song <zihanson@usc.edu>
2021-12-08 22:11:31 -0800

    matche APIs, untested ye

--- index.js
+++ index.js
@@ -42,13 +42,66 @@ class AccountMapper {
     }
 }
 
+class MatchDO {
+    constructor(username, level, player, winner, steps) {
+        this.username = username;
+        this.level = level;
+        this.player = player;
+        this.winner = winner;
+        this.steps = steps;
+        this.timestamp = new Date();
+    }
+}
+
+class MatchVO {
+    constructor(matchDO) {
+        this.id = matchDO._id.toString();
+        this.level = matchDO.level;
+        this.player = matchDO.player;
+        this.winner = matchDO.winner;
+        this.steps = matchDO.steps;
+        this.timestamp = matchDO.timestamp;
+    }
+
+}
+
+class MatchMapper {
+    constructor(mongoDb) {
+        this.db = mongoDb;
+        this.COLLECTION = 'match';
+    }
+
+    async insertMatch(matchDO) {
+        const result = await this.db.collection(this.COLLECTION).insertOne(matchDO);
+        return result;
+    }
+
+    async listMatches(username) {
+        const result = await this.db.collection(this.COLLECTION).find({'username': username}).toArray();
+        return result;
+    }
+
+    // async getAccount(username) {
+    //     let result = await this.db.collection(this.COLLECTION).findOne({'username': username});
+    //     return result;
+    // }
+
+    // async updateToken(username, token) {
+    //     let toUpdate = {
+    //         token: token
+    //     };
+    //     const result = await mongoDb.collection(this.COLLECTION).updateOne({'username': username}, {$set: toUpdate});
+    //     return result;
+    // }
+}
+
 app.use(cors());
 
 app.use(express.json());
 
 app.post('/account/signup', async (req, res, next) => {
-    let username = req.query.username;
-    let password = req.query.password;
+    let username = req.body.username;
+    let password = req.body.password;
 
     let resBody = {
         success: false,
@@ -74,6 +127,10 @@ app.post('/account/signup', async (req, res, next) => {
     const writeRes = await accountMapper.insertAccount(accountDO);
     if (writeRes.acknowledged) {
         resBody.success = true;
+    } else {
+        res.writeHead(502);
+        res.end();
+        return;
     }
     res.writeHead(200, {'Content-Type': 'json'});
     res.write(JSON.stringify(resBody));
@@ -82,8 +139,8 @@ app.post('/account/signup', async (req, res, next) => {
 });
 
 app.post('/account/login', async (req, res, next) => {
-    let username = req.query.username;
-    let password = req.query.password;
+    let username = req.body.username;
+    let password = req.body.password;
 
     let resBody = {
         success: false,
@@ -129,7 +186,7 @@ app.post('/account/login', async (req, res, next) => {
 });
 
 app.post('/account/logout', async (req, res, next) => {
-    let username = req.query.username;
+    let username = req.body.username;
 
     let resBody = {
         success: false,
@@ -184,6 +241,103 @@ app.post('/board/next', async (req, res, next) => {
     return;
 });
 
+app.post('/board/record', async (req, res, next) => {
+    const username = req.body.username;
+    const token = req.body.token;
+    const level = req.body.level;
+    const player = req.body.player;
+    const winner = req.body.winner;
+    const steps = req.body.steps;
+
+    let resBody = {
+        success: false,
+        message: ""
+    };
+
+    if (!username || !token || !level || !player || !winner ||!steps) {
+        res.writeHead(400);
+        res.end();
+        return;
+    }
+    
+    const accountDO = await accountMapper.getAccount(username);
+    if (!accountDO) {
+        resBody.message = "username does not exist";
+        res.writeHead(200, {'Content-Type': 'json'});
+        res.write(JSON.stringify(resBody));
+        res.end();
+        return;
+    }
+    if (accountDO.token != token) {
+        resBody.message = "unmatched token and username";
+        res.writeHead(200, {'Content-Type': 'json'});
+        res.write(JSON.stringify(resBody));
+        res.end();
+        return;
+    }
+
+    let matchDO = new MatchDO(username, level, player, winner, steps);
+
+    const writeRes = await matchMapper.insertMatch(matchDO);
+    if (writeRes.acknowledged) {
+        resBody.success = true;
+    } else {
+        res.writeHead(502);
+        res.end();
+        return;
+    }
+    res.writeHead(200, {'Content-Type': 'json'});
+    res.write(JSON.stringify(resBody));
+    res.end();
+    return;
+});
+
+app.get('/board/record', async (req, res, next) => {
+    let token = req.query.token;
+    let username = req.query.username;
+
+    if (!token || !username) {
+        res.writeHead(400);
+        res.end();
+        return;
+    }
+
+    let matches = [];
+
+    let resBody = {
+        success: false,
+        message: "",
+    }
+
+    const accountDO = await accountMapper.getAccount(username);
+    if (!accountDO) {
+        resBody.message = "username does not exist";
+        res.writeHead(200, {'Content-Type': 'json'});
+        res.write(JSON.stringify(resBody));
+        res.end();
+        return;
+    }
+    if (accountDO.token != token) {
+        resBody.message = "unmatched token and username";
+        res.writeHead(200, {'Content-Type': 'json'});
+        res.write(JSON.stringify(resBody));
+        res.end();
+        return;
+    }
+    
+    let matchDOs = await matchMapper.listMatches(username);
+    for (let i = 0; i < matchDOs.length; i ++) {
+        const matchVO = new MatchVO(matchDOs[i]);
+        matches.append(matchVO);
+    }
+    resBody.success = true;
+    resBody.matches = matches;
+    res.writeHead(200, {'Content-Type': 'json'});
+    res.write(JSON.stringify(resBody));
+    res.end();
+    return;
+});
+
 app.get('*', (req, res, next) => {
     res.writeHead(404);
     res.end();
@@ -193,6 +347,7 @@ app.get('*', (req, res, next) => {
 
 let mongoDb;
 let accountMapper;
+let matchMapper;
 
 const MONGO_FILE_PATH = './config/mongo.json';
 let mongoFileInfo;
@@ -219,6 +374,7 @@ const mongoDatabase = (async () => {
   const mongoConnect = await MongoClient.connect(uri);
   mongoDb = mongoConnect.db(mongoInfo.db);
   accountMapper = new AccountMapper(mongoDb);
+  matchMapper = new MatchMapper(mongoDb);
 
   app.listen(PORT);
   console.log(`Server started, port ${PORT}`);
